76a6d8bfee0791180fdbf4b362af2a29b4f72204
Ajin <ajinjames100@gmail.com>
2025-02-14 22:32:30 +0530

    Added main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int cid;
+    float cr;
+    printf("enter the course id and credit:");
+    scanf("%d%f",&cid,&cr);
+    printf("courseid:%d",cid);
+    printf("\ncredit:%f",cr);
+    return 0;
+
+}
+
